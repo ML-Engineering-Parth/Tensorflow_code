ab85393d9296d52efe8bbbf9b319cae35969dfa9
ML-Engineering-Parth <151672357+ML-Engineering-Parth@users.noreply.github.com>
2023-11-21 14:42:15 -0400

    Update helper_functions_V2.py

--- helper_functions_V2.py
+++ helper_functions_V2.py
@@ -1,2 +1,11 @@
 import tensorflow as tf
 
+def model_prediction_class(model_name, test_data, sample_number):
+  class_list = test_data.class_names 
+  pred_prob = mode_name.predict(test_data)
+  class_index = pre_prob[sample_number].argmax()
+  print(f"Number of prediction probabilities for sample {sample_number} are len(pred_prob[sample_number])")
+  print(f"Shape of prediction probabilities for this model: {pred_prob.shape}")
+  print(f"predicted class index by the model for this sample: {class_index}")
+  print(f"predicted class name by this model for this sample: {class_list[class_index]}")
+
