9939563e9d9bf1f57ec2b5ce0b0c01ebcb0c442c
ML-Engineering-Parth <151672357+ML-Engineering-Parth@users.noreply.github.com>
2023-11-21 14:52:51 -0400

    Update helper_functions_V2.py

--- helper_functions_V2.py
+++ helper_functions_V2.py
@@ -4,7 +4,7 @@ def model_prediction_class(model_name, test_data, sample_number):
   """ Function takes model_name, test_data, and the sample_number as inputs and returns the predicted class_name"""
   class_list = test_data.class_names 
   pred_prob = model_name.predict(test_data)
-  class_index = pre_prob[sample_number].argmax()
+  class_index = pred_prob[sample_number].argmax()
   print(f"Number of prediction probabilities for sample {sample_number} are len(pred_prob[sample_number])")
   print(f"Shape of prediction probabilities for this model: {pred_prob.shape}")
   print(f"predicted class index by the model for this sample: {class_index}")
